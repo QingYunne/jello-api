8aac359da81f0568cd5c47f89caa31b828f083de
Thanh Van Nguyen <qingyunruan1605@gmail.com>
2026-02-14 14:48:09 +0000

    handle validation and handle error

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,24 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/apiError'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(100).trim().strict(),
+    description: Joi.string().required().min(3).max(255).trim().strict()
+  })
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    next()
+  } catch (err) {
+    const customError = new ApiError(
+      StatusCodes.UNPROCESSABLE_ENTITY,
+      err.message
+    )
+    next(customError)
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
